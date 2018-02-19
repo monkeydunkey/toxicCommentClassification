90cc11576c917f4c03f217503dcd40b95a3587c4
monkeydunkey <bhushan.shashank93@gmail.com>
2018-02-18 18:41:02 -0700

    submission creation bug fixed

--- src/GRUModeling.py
+++ src/GRUModeling.py
@@ -89,5 +89,5 @@ model.fit(X_train, y_train, batch_size=64, epochs=5, validation_data=(X_val, y_v
 y_test = model.predict([X_te], batch_size=1024, verbose=1)
 timeStr = str(datetime.now().date()).replace('-', '_') + ' ' + str(datetime.now().time()).replace(':', '_').replace('.', '_')
 sample_submission = pd.DataFrame(data = {"id": test.id.values})
-sample_submission[list_classes] = y_test
+sample_submission = pd.concat([submid, pd.DataFrame(y_test, columns = list_classes)], axis=1)
 sample_submission.to_csv('GRU-submission'+ timeStr +'.csv', index=False)
